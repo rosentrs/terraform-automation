0c9986088a5106290e1222f1178cd40efaa4ee1b
Sophie Rosentreter <rosentreter.sophie@gmail.com>
2023-07-25 14:47:56 +0000

    docker image version

---
+++ dockerimagerversion/main.tf
@@ -0,0 +1,59 @@
+provider "hcloud" {
+  token = var.hcloud_token
+}
+
+resource "hcloud_server" "webserver" {
+  name        = "webserver"
+  image       = "ubuntu-22.04"
+  server_type = "cx11"
+  
+  # Hier weitere Konfigurationen für den Server hinzufügen, falls nötig
+  
+  labels = {
+    environment = "production"
+  }
+}
+
+resource "hcloud_floating_ip" "webserver_ip" {
+  type = "ipv4"
+  server_id = hcloud_server.webserver.id
+}
+
+resource "hcloud_ssh_key" "ssh_key" {
+  name      = "my-ssh-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "hcloud_server_ssh_key" "webserver_ssh_key" {
+  server_id = hcloud_server.webserver.id
+  ssh_key_id = hcloud_ssh_key.ssh_key.id
+}
+
+resource "hcloud_floating_ip_assignment" "webserver_ip_assignment" {
+  floating_ip_id = hcloud_floating_ip.webserver_ip.id
+  server_id = hcloud_server.webserver.id
+}
+
+provider "docker" {
+  # Verwenden Sie hier die Zugangsdaten für Ihre Container-Registry
+  # Alternativ können Umgebungsvariablen oder Dateien für die Authentifizierung verwendet werden
+  # Weitere Informationen finden Sie in der Terraform-Dokumentation für den Docker-Provider
+}
+
+resource "docker_image" "apache_image" {
+  name          = "mein-docker-registry/mein-apache-image"
+  keep_locally  = false
+}
+
+resource "docker_container" "webserver_container" {
+  name  = "webserver_container"
+  image = docker_image.apache_image.latest
+  ports {
+    internal = 80
+    external = 80
+  }
+}
+
+output "server_ip" {
+  value = hcloud_floating_ip.webserver_ip.floating_ip
+}
